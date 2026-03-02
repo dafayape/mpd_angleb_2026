faeddb9aee895ce70fdc88d73d5281955841572c
Daffa Jaya Perkasa <251403746+dafayape@users.noreply.github.com>
2026-03-02 23:33:21 +0700

    feat: add Highcharts exporting and accessibility modules to dashboard.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -655,6 +655,9 @@
 
 @push('js')
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
     <script>
         AOS.init({
